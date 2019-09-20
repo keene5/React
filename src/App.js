6b1fe9e62291facb6a60382857ad7f743e34bfa1
todd keene <todd.keene@libertymutual.com>
2019-09-20 07:42:26 -0400

    My first Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import Contacts from './components/Contacts'
 import './App.css';
+import Header from "./Header";
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="App">
+      <Header branding = "Contact Manager"/>
+      <Contacts name="John Doe"
+      email="jd@gmail.com"
+      phone="555-555-5555"/>
+      <Contacts name="Jane Doe"
+      email="jane@gmail.com" phone="555-552-5555"/>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+const Header = (props) => {
+    //destructure from props
+    const {branding} = props;
+        return (
+
+            <nav className="navbar.navbar-expand-sm.navbar-dark bg-danger mb-3 py-0">
+
+                <div className="container">
+                    <a href="/" className="navbar-brand">
+                        {branding}
+                    </a>
+                    <div>
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                            <a>href="/"classname="nav-link>
+                                Home
+                            </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </nav>
+         //   <div>
+       //         <h1>{branding}</h1>
+
+      //      </div>
+        );
+    };
+//default a value
+
+Header.defaultProps = {
+    branding: 'My Default Header Name'}
+
+Header.propTypes = {
+    branding: PropTypes.string.isRequired
+};
+
+
+export default Header;
